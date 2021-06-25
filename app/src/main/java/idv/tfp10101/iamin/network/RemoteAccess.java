2b7804989d428bd1bd675e35117126f23c074ead
林以樂 <a0989758574@gmail.com>
2021-06-25 19:45:43 +0800

    測試 2021/06/25

--- app/src/main/java/idv/tfp10101/iamin/network/RemoteAccess.java
+++ app/src/main/java/idv/tfp10101/iamin/network/RemoteAccess.java
@@ -19,7 +19,7 @@ import idv.tfp10101.iamin.Constants;
 public class RemoteAccess {
     // 根網址
     public static String URL_SERVER = "http://192.168.1.101:8080/iamin_JavaServlet/";
-
+    //  test
     /**
      * (Json)抓取server資料
      * @param url
